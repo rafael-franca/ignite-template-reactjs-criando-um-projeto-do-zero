b5d28b497eaa997a70b25c8371a2dbebc9c229f5
Rafael França <rafael.franca@ibyte.com.br>
2021-09-17 08:41:51 -0300

    [feat] Add Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './header.module.scss'
+import commonStyles from '../../styles/common.module.scss'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={ commonStyles.container }>
+      <div className={ styles.container }>
+        <Link href="/">
+          <a>
+            <Image src='/spacetraveling.png' alt='logo' width={239} height={27} />
+          </a>
+        </Link>
+      </div>
+    </header>
+  )
 }
